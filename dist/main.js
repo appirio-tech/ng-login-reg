7b9fd39d19abbb973a6b20e016ea0685a20d23bd
samsep <ssepasi@appirio.com>
2015-11-12 09:48:27 -0800

    build

--- dist/main.js
+++ dist/main.js
@@ -10,8 +10,8 @@
 
 angular.module("appirio-tech-ng-login-reg").run(["$templateCache", function($templateCache) {$templateCache.put("views/forgot-password.directive.html","<p class=\"success\">Thanks, we\'ve emailed you a reset link. Please check your inbox. If you still need help, please contact us at support@topcoder.com</p><form ng-submit=\"vm.submit()\" method=\"post\"><input type=\"text\" ng-model=\"vm.email\" required=\"required\" placeholder=\"email\" autofocus=\"autofocus\" class=\"widest\"/><button type=\"submit\" class=\"action\">send reset link</button></form>");
 $templateCache.put("views/login-reg-shell.directive.html","<header class=\"flex center middle\"><img src=\"/images/asp.svg\"/></header><main ng-if=\"vm.flow == \'LOGIN\'\" class=\"elevated-bottom\"><h1>welcome back to appirio</h1><p class=\"secondary\">Let\'s start building your app.  Login or create your account below to begin.</p><login></login></main><footer ng-if=\"vm.flow == \'LOGIN\'\"><p>Don\'t have an account? <a ui-sref=\"register\">Register Now</a></p></footer><main ng-if=\"vm.flow == \'REGISTRATION\'\" class=\"elevated-bottom\"><h1>welcome to appirio</h1><p class=\"secondary\">Let\'s start building your app.  Create your account below to begin.</p><registration></registration></main><footer ng-if=\"vm.flow == \'REGISTRATION\'\"><p>Already have an account? <a ui-sref=\"login\">Login Here</a></p></footer><main ng-if=\"vm.flow == \'REGISTRATION_SUCCESS\'\" class=\"elevated-bottom\"><p>Thanks for creating an account.  We\'ve sent you a confirmation link.  Please check your email and click the link to activate your account. If you can\'t find the message please contact <a href=\"mailto:support@asp.com\">support@asp.com</a></p></main><main ng-if=\"vm.flow == \'FORGOT\'\" class=\"elevated-bottom\"><h1>reset password</h1><p class=\"secondary\">Enter your email and we\'ll send you a link to reset your password.</p><forgot-password></forgot-password></main><footer ng-if=\"vm.flow == \'FORGOT\'\"><p><a ui-sref=\"login\">Back to Login</a></p></footer><main ng-if=\"vm.flow == \'RESET\'\" class=\"elevated-bottom\"><h1>reset password</h1><p class=\"secondary\">Enter your email and we\'ll send you a link to reset your password.</p><reset-password></reset-password></main><footer ng-if=\"vm.flow == \'RESET\'\"><p><a ui-sref=\"login\">Back to Login</a></p></footer>");
-$templateCache.put("views/login.directive.html","<p ng-class=\"{ hide: !vm.error }\" class=\"error transition\">We can\'t find an account with the email / password you entered. Please try again.</p><form ng-submit=\"vm.submit()\" method=\"post\" class=\"flex column middle\"><input type=\"text\" ng-model=\"vm.username\" required=\"required\" placeholder=\"username\" autofocus=\"autofocus\" class=\"widest\"/><input type=\"password\" ng-model=\"vm.password\" required=\"required\" placeholder=\"password\" class=\"widest\"/><div class=\"forgot\"><a ui-sref=\"RESET_PASSWORD\">Forgot Password?</a></div><button type=\"submit\" class=\"action\">login</button></form>");
-$templateCache.put("views/registration.directive.html","<p ng-class=\"{ hide: !vm.error }\" class=\"error\">{{ vm.errorMessage }}</p><form ng-submit=\"vm.submit()\"><div class=\"first-name\"><input type=\"text\" name=\"first-name\" ng-model=\"vm.firstName\" required=\"required\" placeholder=\"First Name\" maxlength=\"64\" autofocus=\"autofocus\" class=\"widest\"/></div><div class=\"last-name\"><input type=\"text\" name=\"last-name\" ng-model=\"vm.lastName\" required=\"required\" placeholder=\"Last Name\" maxlength=\"64\" class=\"widest\"/></div><div class=\"organization\"><input type=\"text\" name=\"organization\" ng-model=\"vm.organization\" required=\"required\" placeholder=\"Organization\" class=\"widest\"/></div><hr/><input type=\"text\" name=\"username\" ng-model=\"vm.username\" required=\"required\" placeholder=\"Username\" class=\"widest\"/><input type=\"password\" name=\"password\" ng-model=\"vm.password\" required=\"required\" placeholder=\"Password\" class=\"widest\"/><input type=\"email\" name=\"email\" ng-model=\"vm.email\" required=\"required\" placeholder=\"Email\" class=\"widest\"/><button type=\"submit\" class=\"action submit\">Register</button></form>");
+$templateCache.put("views/login.directive.html","<p ng-class=\"{ hide: !vm.error }\" class=\"error transition\">We can\'t find an account with the email / password you entered. Please try again.</p><loader ng-if=\"vm.loading\"></loader><form ng-submit=\"vm.submit()\" method=\"post\" class=\"flex column middle\"><input type=\"text\" ng-model=\"vm.username\" required=\"required\" placeholder=\"username\" autofocus=\"autofocus\" class=\"widest\"/><input type=\"password\" ng-model=\"vm.password\" required=\"required\" placeholder=\"password\" class=\"widest\"/><div class=\"forgot\"><a ui-sref=\"RESET_PASSWORD\">Forgot Password?</a></div><button type=\"submit\" class=\"action\">login</button></form>");
+$templateCache.put("views/registration.directive.html","<p ng-class=\"{ hide: !vm.error }\" class=\"error\">{{ vm.errorMessage }}</p><loader ng-if=\"vm.loading\"></loader><form ng-submit=\"vm.submit()\"><div class=\"first-name\"><input type=\"text\" name=\"first-name\" ng-model=\"vm.firstName\" required=\"required\" placeholder=\"First Name\" maxlength=\"64\" autofocus=\"autofocus\" class=\"widest\"/></div><div class=\"last-name\"><input type=\"text\" name=\"last-name\" ng-model=\"vm.lastName\" required=\"required\" placeholder=\"Last Name\" maxlength=\"64\" class=\"widest\"/></div><div class=\"organization\"><input type=\"text\" name=\"organization\" ng-model=\"vm.organization\" required=\"required\" placeholder=\"Organization\" class=\"widest\"/></div><hr/><input type=\"text\" name=\"username\" ng-model=\"vm.username\" required=\"required\" placeholder=\"Username\" class=\"widest\"/><input type=\"password\" name=\"password\" ng-model=\"vm.password\" required=\"required\" placeholder=\"Password\" class=\"widest\"/><input type=\"email\" name=\"email\" ng-model=\"vm.email\" required=\"required\" placeholder=\"Email\" class=\"widest\"/><button type=\"submit\" class=\"action submit\">Register</button></form>");
 $templateCache.put("views/reset-password.directive.html","<p class=\"success\">Your password has been updated.</p><form ng-submit=\"vm.submit()\" method=\"post\"><input type=\"password\" ng-model=\"vm.email\" required=\"required\" placeholder=\"New Password\" autofocus=\"autofocus\" class=\"widest\"/><button type=\"submit\" class=\"action\">reset password</button></form>");
 $templateCache.put("views/sso-callback.directive.html","<p>SSO Callback Directive. Now redirecting...</p>");
 $templateCache.put("views/sso-login.directive.html","<p>SSO Login Directive. Now redirecting...</p>");}]);
@@ -176,9 +176,11 @@ $templateCache.put("views/sso-login.directive.html","<p>SSO Login Directive. Now
     vm.password = '';
     vm.error = false;
     vm.submit = null;
+    vm.loading = false;
     vm.submit = function() {
       var loginOptions;
       vm.error = false;
+      vm.loading = true;
       loginOptions = {
         username: vm.username,
         password: vm.password,
@@ -188,10 +190,12 @@ $templateCache.put("views/sso-login.directive.html","<p>SSO Login Directive. Now
       return AuthService.login(loginOptions);
     };
     loginFailure = function(error) {
-      return vm.error = true;
+      vm.error = true;
+      return vm.loading = false;
     };
     loginSuccess = function() {
       vm.error = false;
+      vm.loading = false;
       return UserV3Service.loadUser().then(function(currentUser) {
         var preAuthState, urlToken;
         urlToken = $location.search();
@@ -237,10 +241,12 @@ $templateCache.put("views/sso-login.directive.html","<p>SSO Login Directive. Now
     vm.error = false;
     vm.errorMessage = 'Error Creating User';
     vm.submit = null;
+    vm.loading = false;
     vm.submit = function() {
       var afterActivationURL, registerOptions;
       vm.error = false;
-      afterActivationURL = $state.href('view-work-multiple', [], {
+      vm.loading = true;
+      afterActivationURL = $state.href('login', [], {
         absolute: true
       });
       registerOptions = {
@@ -253,6 +259,7 @@ $templateCache.put("views/sso-login.directive.html","<p>SSO Login Directive. Now
     };
     registerError = function(error) {
       vm.error = true;
+      vm.loading = false;
       return vm.errorMessage = error.data.result.content;
     };
     success = function() {
@@ -261,6 +268,7 @@ $templateCache.put("views/sso-login.directive.html","<p>SSO Login Directive. Now
     registerSuccess = function() {
       var loginOptions;
       vm.error = false;
+      vm.loading = false;
       $state.go('registration-success');
       loginOptions = {
         username: vm.username,
